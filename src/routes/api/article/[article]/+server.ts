00064cfaf01bef6f1262d8865df76fa45c7f1408
David Smadja <david.smadja@assemblee-nationale.fr>
2025-05-02 14:15:47 +0200

    Improve article API

--- src/routes/api/article/[article]/+server.ts
+++ src/routes/api/article/[article]/+server.ts
@@ -1,13 +1,19 @@
-import { json } from "@sveltejs/kit"
+import { error, json } from "@sveltejs/kit"
 import type { RequestHandler } from "./$types"
 
 export const GET: RequestHandler = async ({ params, locals }) => {
 	const { article } = params as { article: string }
 	const { sql } = locals
-	const test: JSON = await sql<JSON>`
+	const articleTxtFromDb: JSON[] = await sql<JSON>`
 		select data
 		from article
 		where id=${article}`
 
-	return json(test)
+	if (articleTxtFromDb.length === 1) {
+		return json(articleTxtFromDb[0])
+	} else if (articleTxtFromDb.length === 0) {
+		error(404, `Article not found`)
+	} else {
+		error(422, `Error : article ID refers to multiple articles`)
+	}
 }
