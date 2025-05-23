dac33017d2c5eec23df850d3bdf128eee56dbd57
David Smadja <david.smadja@assemblee-nationale.fr>
2025-05-23 11:34:53 +0200

    format

--- src/routes/bill/[bill]/+page.svelte
+++ src/routes/bill/[bill]/+page.svelte
@@ -10,9 +10,9 @@
 	let billHTML = $state(data.billHTML)
 </script>
 
-<div class="flex min-h-full w-full flex-row fixed overflow-hidden">
+<div class="fixed flex min-h-full w-full flex-row overflow-hidden">
 	<div class="h-screen w-1/2 overflow-y-auto"><Bill {billHTML}></Bill></div>
-	<div class="w-1/2 bg-amber-200 h-screen overflow-y-auto">
+	<div class="h-screen w-1/2 overflow-y-auto bg-amber-200">
 		<p>Ici la loi</p>
 		<p>lorem ipsum</p>
 	</div>
