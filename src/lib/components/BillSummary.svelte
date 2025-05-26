fa540c979f3f62ae14912fab8148d3c7954bf303
David Smadja <david.smadja@assemblee-nationale.fr>
2025-05-26 14:20:42 +0200

    Add iconify-icon and adjust summary layout

--- src/lib/components/BillSummary.svelte
+++ src/lib/components/BillSummary.svelte
@@ -46,19 +46,21 @@
 	})
 </script>
 
-<div class="sticky top-0 z-10 bg-white p-4 shadow-md">
-	<button
-		class="w-full bg-gray-100 px-4 py-2 text-left font-medium hover:bg-gray-200"
-		onclick={() => (summaryIsOpen = !summaryIsOpen)}
-	>
-		Sommaire
-		<span
-			class="float-right transform transition-transform"
-			class:rotate-180={summaryIsOpen}
+<div class="sticky top-0 z-10 p-4 shadow-md">
+	<div class="flex items-center justify-between">
+		<span>test 1235 sdjh</span>
+		<button
+			class="flex items-center px-4 py-2 font-medium text-gray-500 uppercase"
+			onclick={() => (summaryIsOpen = !summaryIsOpen)}
 		>
-			▼
-		</span>
-	</button>
+			<iconify-icon
+				class="mr-1 align-[-0.25rem] text-xl"
+				icon="ri:menu-fold-2-line"
+			>
+			</iconify-icon>
+			Sommaire
+		</button>
+	</div>
 	<ul
 		class="overflow-y-auto transition-all duration-300 ease-in-out"
 		class:max-h-0={!summaryIsOpen}
