3e9b2d4212b51c1c6b7b82ccea7eb07d831dc69d
David Smadja <david.smadja@assemblee-nationale.fr>
2025-06-02 15:47:41 +0200

    style navbar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,6 +1,10 @@
 <div
-	class="fixed bottom-0 flex w-full flex-row flex-nowrap justify-items-center-safe overflow-hidden bg-emerald-400"
+	class="fixed bottom-0 flex w-full flex-row flex-nowrap justify-items-center-safe overflow-hidden bg-white py-5 text-center shadow-[0px_-5px_5px_-5px_rgba(0,0,0,0.25)]"
 >
-	<div class="basis-1/2 text-center">Projet de loi</div>
-	<div class="basis-1/2 text-center">Loi</div>
+	<div class="basis-1/2">
+		<button class="rounded-full border-4 px-12 uppercase">Projet de loi</button>
+	</div>
+	<div class="basis-1/2">
+		<button class="rounded-full border-4 px-12 uppercase">Loi</button>
+	</div>
 </div>
