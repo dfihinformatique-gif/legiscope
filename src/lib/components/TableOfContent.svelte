61d6ab4bd82ebe9d0b630aa3268da62ddbbbd8e4
David Smadja <david.smadja@assemblee-nationale.fr>
2025-06-02 14:59:57 +0200

    remove unused component

--- src/lib/components/TableOfContent.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { displayedArticle } from "$lib/stores/article"
-	const article = $displayedArticle
-</script>
-
-{#if article}
-	{article.CONTEXTE.TEXTE.TITRE_TXT[0]["#text"]}
-{/if}
-<div></div>
