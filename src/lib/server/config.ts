f4b738f6372ac16fc23ba95f5f4ca571d0aff4db
David Smadja <david.smadja@assemblee-nationale.fr>
2025-08-22 14:52:29 +0200

    Adapt to zod 4 syntax

--- src/lib/server/config.ts
+++ src/lib/server/config.ts
@@ -3,34 +3,34 @@ import { z } from "zod"
 
 const DatabaseSchema = z.object({
 	host: z.string({
-		required_error:
-			"PG_HOST is required in .env - Modify .env and reload server",
-		invalid_type_error:
-			"PG_HOST must be a string in .env - Modify .env and reload server",
+		error: (issue) =>
+			issue.input === undefined
+				? "PG_HOST is required in .env - Modify .env and reload server"
+				: "PG_HOST must be a string in .env - Modify .env and reload server",
 	}),
 	database: z.string({
-		required_error:
-			"PG_DATABASE is required in .env - Modify .env and reload server",
-		invalid_type_error:
-			"PG_DATABASE must be a string in .env - Modify .env and reload server",
+		error: (issue) =>
+			issue.input === undefined
+				? "PG_DATABASE is required in .env - Modify .env and reload server"
+				: "PG_DATABASE must be a string in .env - Modify .env and reload server",
 	}),
 	port: z.number({
-		required_error:
-			"PG_PORT is required in .env - Modify .env and reload server",
-		invalid_type_error:
-			"PG_PORT must be a number in .env - Modify .env and reload server",
+		error: (issue) =>
+			issue.input === undefined
+				? "PG_PORT is required in .env - Modify .env and reload server"
+				: "PPG_PORT must be a number in .env - Modify .env and reload server",
 	}),
 	username: z.string({
-		required_error:
-			"PG_USERNAME is required in .env - Modify .env and reload server",
-		invalid_type_error:
-			"PG_USERNAME must be a string in .env - Modify .env and reload server",
+		error: (issue) =>
+			issue.input === undefined
+				? "PG_USERNAME is required in .env - Modify .env and reload server"
+				: "PG_USERNAME must be a string in .env - Modify .env and reload server",
 	}),
 	password: z.string({
-		required_error:
-			"PG_PASSWORD is required in .env - Modify .env and reload server",
-		invalid_type_error:
-			"PG_PASSWORD must be a string in .env - Modify .env and reload server",
+		error: (issue) =>
+			issue.input === undefined
+				? "PG_PASSWORD is required in .env - Modify .env and reload server"
+				: "PG_PASSWORD must be a string in .env - Modify .env and reload server",
 	}),
 })
 
