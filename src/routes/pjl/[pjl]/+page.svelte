27520c5b36089afae13d7761fe12dbc01df1887c
David Smadja <david.smadja@assemblee-nationale.fr>
2025-09-08 10:43:13 +0200

    remove unnecessary comments

--- src/routes/pjl/[pjl]/+page.svelte
+++ src/routes/pjl/[pjl]/+page.svelte
@@ -17,28 +17,19 @@
 	let lawArticle = $derived(page.url.searchParams.get("lawArticle") || "")
 	let pjlHTML = $state(data.pjlHTML)
 
-	// Permet que de réintialiser le scroll de la vue lax lorsqu'on a basculé d'onglet, et qu'on reclique sur un lien
 	let lawContainer: HTMLDivElement | undefined = $state()
-
-	// let shouldResetLawScroll = $state(false)
 
 	$effect(() => {
 		if (shared.activePanelMobile === "law" && lawContainer !== undefined) {
-			// requestAnimationFrame(() => {
-			// 	if (shouldResetLawScroll) {
 			lawContainer.scrollTo({ top: 0, behavior: "auto" })
-			// shouldResetLawScroll = false
 		}
 	})
-	// }
-	// })
 
 	// Permet d'afficher l'article dans la vue law quand on clique sur un lien depuis la bill
 	$effect(() => {
 		page.url
 		if (lawArticle) {
 			isFetchingArticle = true
-			// const todayIso = new Date().toISOString().split("T")[0]
 			fetch(`/api/article/${lawArticle}/${shared.pjlDate}`)
 				.then((res) => (res.ok ? res.json() : null))
 				.then((data) => {
@@ -48,13 +39,9 @@
 					associatedText = data.text
 					associatedTextTitle = data.textTitle ?? ""
 					if (shared.isMobilePhone) {
-						// Mobile : Change de vue vers law et reset le scroll
-						// shouldResetLawScroll = true
 						shared.activePanelMobile = "law"
 					} else {
-						// Desktop - vue unique sur bill : Ouvre la vue law
 						shared.showLawDesktop = true
-						// shouldResetLawScroll = true
 					}
 				})
 				.catch(() => (lawArticle = ""))
