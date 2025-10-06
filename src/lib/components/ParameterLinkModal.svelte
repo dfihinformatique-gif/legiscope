912f76c1b0c7c4bf669ccb293b6cf75f96522c4d
Dorine Lambinet <dorine.lambinet@gmail.com>
2025-10-06 11:08:49 +0200

    Réutilise la modale de socio-fiscal - crée les composants associés

--- src/lib/components/ParameterLinkModal.svelte
+++ src/lib/components/ParameterLinkModal.svelte
@@ -1,28 +1,54 @@
-<script>
-	let {
-		showParameterModal = $bindable(),
-		parametersToVariables = $bindable(),
-		children,
-	} = $props()
+<script lang="ts">
+	import DialogContent from "$lib/components/ui_transverse_components/DialogContent.svelte"
+	import DialogOverlay from "$lib/components/ui_transverse_components/DialogOverlay.svelte"
+	import { Dialog } from "bits-ui"
+	import { onMount } from "svelte"
 
-	let dialog = $state() // HTMLDialogElement
+	// Props
+	let { showParameterModal = $bindable(false), children, onClose } = $props()
 
-	$effect(() => {
-		if (showParameterModal) dialog.showModal()
+	// Référence au bouton fermer
+	let closeButton: HTMLButtonElement
+
+	// Focus programmatique pour l'accessibilité
+	onMount(() => {
+		if (showParameterModal) {
+			closeButton?.focus()
+		}
 	})
+
+	function closeModal() {
+		showParameterModal = false
+		onClose?.()
+	}
 </script>
 
-<dialog
-	bind:this={dialog}
-	onclose={() => (showParameterModal = false)}
-	onclick={(e) => {
-		if (e.target === dialog) dialog.close()
-	}}
->
-	<div>
-		{@render children?.()}
-		<hr />
-		<!-- svelte-ignore a11y_autofocus -->
-		<button autofocus onclick={() => dialog.close()}>Fermer</button>
-	</div>
-</dialog>
+<Dialog.Root bind:open={showParameterModal}>
+	<Dialog.Portal>
+		<DialogOverlay
+			class="fixed inset-0 z-50 bg-gray-500 opacity-50 transition-opacity"
+		/>
+
+		<DialogContent
+			class="fixed top-1/2 left-1/2 z-50 flex max-h-[85%] w-full max-w-md -translate-x-1/2 -translate-y-1/2 transform flex-col overflow-hidden rounded-md bg-white p-6 text-left shadow-xl transition-all"
+		>
+			<!-- Bouton Fermer -->
+			<div class="flex justify-end">
+				<button
+					bind:this={closeButton}
+					class="flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg p-1.5 hover:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:outline-none active:bg-gray-200"
+					onclick={closeModal}
+				>
+					<iconify-icon class="h-6 w-6 text-2xl" icon="ri-close-line"
+					></iconify-icon>
+					<span class="sr-only">Fermer la fenêtre</span>
+				</button>
+			</div>
+
+			<!-- Contenu de la modale -->
+			<div class="overflow-y-auto">
+				{@render children?.()}
+			</div>
+		</DialogContent>
+	</Dialog.Portal>
+</Dialog.Root>
