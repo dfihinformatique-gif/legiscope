a4ef1595494dffa68dea923df2da2eadb1c7778f
David Smadja <david.smadja@assemblee-nationale.fr>
2025-10-09 17:49:02 +0200

    don't use deprecated option

--- svelte.config.js
+++ svelte.config.js
@@ -3,23 +3,24 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-  compilerOptions: { runes: true },
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	compilerOptions: { runes: true },
 
-  kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter(),
 
-    csrf: {
-      // Disabling is required for authentication.
-      // But CSRF check is done by csrfHandler.
-      checkOrigin: false,
-    },
-  },
+		csrf: {
+			// Disabling is required for authentication.
+			// But CSRF check is done by csrfHandler.
+			trustedOrigins:
+				process.env.NODE_ENV === "development" ? ["http://localhost:5173"] : [],
+		},
+	},
 }
 
 export default config
