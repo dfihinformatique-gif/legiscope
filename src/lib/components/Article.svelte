40c39505d2dc8083d3820d3d984e13bcd90253d0
Dorine Lambinet <dorine.lambinet@gmail.com>
2025-12-04 11:33:50 +0100

    Ajoute un message d'information pour indiquer que les citations ne sont pas complètement fiables

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -962,6 +962,20 @@
 					class:border-gray-200={citationsIsOpen}
 				>
 					<ArticleCitations {articleInfo}></ArticleCitations>
+					<div
+						class="mt-4 rounded-sm border-2 border-yellow-600 bg-amber-50 p-2 text-sm text-yellow-900"
+					>
+						<iconify-icon
+							icon="ri-information-fill"
+							class="mr-1 align-[-0.2rem] text-base text-yellow-600"
+						></iconify-icon>
+						<b
+							>Certaines versions ne citent pas cet article ? Il manque des
+							citations ?</b
+						>
+						Le Légiscope s'appuie sur la liste des citations mise à disposition par
+						Légifrance. Cette liste peut contenir des erreurs ou des manques.
+					</div>
 				</div>
 			{/if}
 
