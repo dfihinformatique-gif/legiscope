08b414dfc83bca398591c50fb037975a5233c1e4
Emmanuel Raviart <emmanuel@raviart.com>
2025-12-15 22:55:50 +0100

    Ajout du PLF adopté par le Sénat propose en 2ème lecture à l'Assemblée

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,6 +50,25 @@
 							></iconify-icon>
 						</a>
 					</div>
+					<div class="flex justify-start">
+						<a
+							class="hover:border-le-gris-dispositif-dark mt-5 items-center gap-2 rounded-md border-2 border-black bg-white px-5 py-2 font-sans text-lg font-bold tracking-[0.085em] text-black shadow-lg hover:text-blue-950 active:bg-neutral-200"
+							href="/pjl/tas25-029"
+							onclick={() => (isLoadingPjl = true)}
+							title="Vers le projet de loi"
+						>
+							PLFSS 2026 <span
+								class="rounded-md border bg-blue-200 p-1 text-sm font-normal tracking-normal"
+								>Version déposée à l'Assemblée nationale, le 15/12/2025 pour
+								deuxième lecture</span
+							>
+							<iconify-icon
+								class="ml-2 align-[-0.4rem] text-2xl"
+								icon="ri-arrow-right-line"
+							></iconify-icon>
+						</a>
+					</div>
+
 					<h2 class="mt-12 font-serif">
 						Projet de loi de financement de la sécurité sociale :
 					</h2>
@@ -123,6 +142,7 @@
 							></iconify-icon>
 						</a>
 					</div>
+
 					<h2 class="mt-12 font-serif">
 						Projet de loi relatif à la lutte contre les fraudes sociales et
 						fiscales :
