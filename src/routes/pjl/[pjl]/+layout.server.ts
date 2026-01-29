975e4a12e754d824d1fb819026d40591f0f54ca7
David Smadja <david.smadja@assemblee-nationale.fr>
2026-01-29 10:49:38 +0100

    Move svg icon in +layout.server

--- src/routes/pjl/[pjl]/+layout.server.ts
+++ src/routes/pjl/[pjl]/+layout.server.ts
@@ -332,9 +332,9 @@ export const load: LayoutServerLoad = async ({
 							parameter.short_label?.replace("'", " "),
 						)
 					}
-					return `<a title='${referredParametersLabels.join("|")}' class='law-article-link' href='/pjl/${pjl}?article=${lawArticle}'>${p2}*</a>`
+					return `<a title='${referredParametersLabels.join("|")}' class='law-article-link' href='/pjl/${pjl}?article=${lawArticle}'><svg class="law-article-icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M20 22H6.5A3.5 3.5 0 0 1 3 18.5V5a3 3 0 0 1 3-3h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1m-1-2v-3H6.5a1.5 1.5 0 0 0 0 3zM10 4v8l3.5-2l3.5 2V4z"></path></svg>${p2}*</a>`
 				} else {
-					return `<a class='law-article-link' href='/pjl/${pjl}?article=${lawArticle}'>${p2}</a>`
+					return `<a class='law-article-link' href='/pjl/${pjl}?article=${lawArticle}'><svg class="law-article-icon" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M20 22H6.5A3.5 3.5 0 0 1 3 18.5V5a3 3 0 0 1 3-3h14a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1m-1-2v-3H6.5a1.5 1.5 0 0 0 0 3zM10 4v8l3.5-2l3.5 2V4z"></path></svg>${p2}</a>`
 				}
 			},
 		)
