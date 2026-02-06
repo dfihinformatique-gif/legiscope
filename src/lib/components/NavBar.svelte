60eb2dcde4da3c989153d838e574d09cf3760f0b
David Smadja <david.smadja@assemblee-nationale.fr>
2026-02-06 16:12:54 +0100

    prefer canonical classes

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -99,9 +99,9 @@
 			<div class="flex h-full flex-1">
 				<button
 					class={`flex w-full items-center justify-center border-b-[6px] border-transparent text-base tracking-wide uppercase sm:text-xl md:text-lg
-    			${shared.activePanelMobile === "bill" ? "!border-le-gris-dispositif-dark text-le-gris-dispositif-dark bg-white font-bold" : ""}
-    			${shared.activePanelMobile === "law" ? "hover:!border-le-gris-dispositif hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
-					${shared.activePanelMobile === "citing" ? "hover:!border-le-gris-dispositif hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
+    			${shared.activePanelMobile === "bill" ? "border-le-gris-dispositif-dark! text-le-gris-dispositif-dark bg-white font-bold" : ""}
+    			${shared.activePanelMobile === "law" ? "hover:border-le-gris-dispositif! hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
+					${shared.activePanelMobile === "citing" ? "hover:border-le-gris-dispositif! hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
   				`}
 					onclick={() => (shared.activePanelMobile = "bill")}
 				>
@@ -111,9 +111,9 @@
 			<div class="flex h-full flex-1">
 				<button
 					class={`flex w-full items-center justify-center border-b-[6px] border-transparent text-base tracking-wide text-black uppercase sm:text-xl md:text-lg
-					${shared.activePanelMobile === "law" ? "!border-le-gris-dispositif-dark text-le-gris-dispositif-dark bg-white font-bold" : ""}
-					${shared.activePanelMobile === "bill" ? "hover:!border-le-gris-dispositif hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
-					${shared.activePanelMobile === "citing" ? "hover:!border-le-gris-dispositif hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
+					${shared.activePanelMobile === "law" ? "border-le-gris-dispositif-dark! text-le-gris-dispositif-dark bg-white font-bold" : ""}
+					${shared.activePanelMobile === "bill" ? "hover:border-le-gris-dispositif! hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
+					${shared.activePanelMobile === "citing" ? "hover:border-le-gris-dispositif! hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
 				`}
 					onclick={() => (shared.activePanelMobile = "law")}
 				>
@@ -124,9 +124,9 @@
 				<div class="flex h-full flex-1">
 					<button
 						class={`flex w-full items-center justify-center border-b-[6px] border-transparent text-base tracking-wide text-black uppercase sm:text-xl md:text-lg
-					${shared.activePanelMobile === "citing" ? "!border-le-gris-dispositif-dark text-le-gris-dispositif-dark bg-white font-bold" : ""}
-					${shared.activePanelMobile === "law" ? "hover:!border-le-gris-dispositif hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
-					${shared.activePanelMobile === "bill" ? "hover:!border-le-gris-dispositif hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
+					${shared.activePanelMobile === "citing" ? "border-le-gris-dispositif-dark! text-le-gris-dispositif-dark bg-white font-bold" : ""}
+					${shared.activePanelMobile === "law" ? "hover:border-le-gris-dispositif! hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
+					${shared.activePanelMobile === "bill" ? "hover:border-le-gris-dispositif! hover:text-le-gris-dispositif bg-neutral-200 text-gray-600  hover:bg-white hover:font-bold" : ""}
 
 				`}
 						onclick={() => (shared.activePanelMobile = "citing")}
