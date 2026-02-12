bc2b1a1baaab87dfd88ca8b6a8e9d645344d8cd4
Dorine Lambinet <dorine.lambinet@gmail.com>
2026-02-12 13:44:23 +0100

    Ajoute du texte caché dans les skeletons pour que le composant ait la largeur de la cible

--- src/lib/components/SkeletonArticleLoader.svelte
+++ src/lib/components/SkeletonArticleLoader.svelte
@@ -4,9 +4,14 @@
 
 <div class="w-full animate-pulse">
 	<div
-		class="mx-3 h-screen max-w-6xl space-y-6 bg-blue-50 p-6 pt-4 shadow md:mx-6"
+		class="mx-3 h-screen max-w-6xl space-y-6 bg-blue-50 p-6 pt-4 text-blue-50 shadow md:mx-6"
 		class:md:p-16={!shared.showBillDesktop}
 	>
+		Curabitur dictum finibus ante quis faucibus. Sed viverra neque nec arcu
+		iaculis, a placerat nunc imperdiet. Nam quis sagittis felis. Phasellus
+		vulputate sodales lacus, maximus accumsan nibh vestibulum nec. Sed elementum
+		non sapien in bibendum. Curabitur in imperdiet felis. Donec dapibus sapien
+		vel tortor sollicitudin malesuada.
 		<div class="bg-le-gris-dispositif-light h-6 w-1/3 rounded"></div>
 		<!-- Titre -->
 		<div class="flex gap-4">
